58eeabbed31ccf6bc00ad3569a640dff1b4d4f8d
Elisha Windham <ewindham@pcsdk12.com>
2022-03-11 08:46:55 -0600

    Initial Commit

---
+++ PCSD App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PCSD App
+//
+//  Created by Elisha Windham on 3/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+   VStack
+    var body: some View {
+        Text("PCSD")
+            .fontWeight(.bold)
+            .navigationTitle(<#T##title: Text##Text#>)
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PCSD App/PCSD_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PCSD_AppApp.swift
+//  PCSD App
+//
+//  Created by Elisha Windham on 3/11/22.
+//
+
+import SwiftUI
+
+@main
+struct PCSD_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
